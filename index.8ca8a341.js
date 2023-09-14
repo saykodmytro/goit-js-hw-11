826fd6f8abd293acbc91f9003ad582f56ccd28a5
saykodmytro <64913341+saykodmytro@users.noreply.github.com>
2023-09-14 10:30:07 +0000

    Deploying to gh-pages from @ saykodmytro/goit-js-hw-11@9d9af34137d5335a2c00117bb23ffd4cc00a524c 🚀

---
+++ index.8ca8a341.js
@@ -0,0 +1,2 @@
+console.log("ggg"),console.log("ggg"),console.log("ggg");
+//# sourceMappingURL=index.8ca8a341.js.map
---
+++ index.be4dda5d.js
@@ -0,0 +1,2 @@
+console.log("ggg"),console.log("ggg"),console.log("ggg");
+//# sourceMappingURL=index.be4dda5d.js.map
