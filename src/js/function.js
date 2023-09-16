f5dfca0965f1deed51eaad1855830aee0b4db4e0
Dmytro Sayko <saykodmytro@icloud.com>
2023-09-16 10:55:07 +0300

    Update function.js

--- src/js/function.js
+++ src/js/function.js
@@ -7,6 +7,7 @@ export function messageTotalPhoto(obj) {
 
 export function onError() {
   loaderEl.classList.remove('active');
+  console.log('remove-loader');
   return Notify.failure(
     'Sorry, there are no images matching your search query. Please try again.'
   );
